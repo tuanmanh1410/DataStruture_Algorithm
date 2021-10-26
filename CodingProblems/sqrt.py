3546c715bcf9276db1bb2722fe1e812dbfa965d4
Tào Tuấn Mạnh <tuanmanh.bka@gmail.com>
2021-10-26 19:34:44 +0900

    Add new file square root

---
+++ CodingProblems/sqrt.py
@@ -0,0 +1,23 @@
+# Find Sqrt() without math function 
+
+class My_Number():
+	def __init__(self, number):
+		self.num = number
+
+	def Sqrt(self):
+		limit = 0.0001
+		upper = self.num
+		lower = 0
+		res = 1
+
+		while (abs(res*res - self.num) > limit):
+			res = (upper + lower)/2
+			# Reduce the gap of upper and lower bound; get the average to get the sqrt(x)
+			if (res*res >= self.num):
+				upper = res
+			else:
+				lower = res
+		return res
+
+a = My_Number(5)
+print(a.Sqrt())
